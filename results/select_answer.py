e511f8eadffe990923cfead5affa22c0f0cc064f
HuihuiChyan <2745480383@qq.com>
2025-07-16 12:32:15 +0000

    add serper

--- results/select_answer.py
+++ results/select_answer.py
@@ -1,12 +1,17 @@
 import pdb
 import json
-with open("fact_checking_dataset_2025_agentic.jsonl", "r") as fin1,\
-open("fact_checking_dataset_2025_baseline.jsonl", "r") as fin2:
+with open("hotpotqa_subset_judged_Qwen2.5-7B-Instruct_agentic.jsonl", "r") as fin1,\
+open("hotpotqa_subset_judged_Qwen2.5-7B-Instruct_baseline.jsonl", "r") as fin2:
     lines1 = [json.loads(line.strip()) for line in fin1.readlines()]
     lines2 = [json.loads(line.strip()) for line in fin2.readlines()]
+    error_count = 0
     for line1, line2 in zip(lines1, lines2):
-        if line1["final_verdict"].lower() == line1['label'] and \
-            line2["final_verdict"].lower() != line2['label']:
+        # if line1["final_verdict"].lower() == line1['label'] and \
+        #     line2["final_verdict"].lower() != line2['label']:
+        #     print(json.dumps(line1['fact_checking_trace'], indent=4))
+        #     print(json.dumps(line2['fact_checking_trace'], indent=4))
+        #     import pdb;pdb.set_trace()
+        if line1["final_verdict"].lower() != line1['label']:
             print(json.dumps(line1['fact_checking_trace'], indent=4))
-            print(json.dumps(line2['fact_checking_trace'], indent=4))
             import pdb;pdb.set_trace()
+        print(f"Final error count {error_count}")
--- run_baseline.sh
+++ run_baseline.sh
@@ -2,13 +2,13 @@ export CUDA_VISIBLE_DEVICES=6
 
 echo "Running baseline fact-checking (without search)..."
 YEAR=2017
-MODEL=Qwen2.5-3B-Instruct
+MODEL=Qwen2.5-7B-Instruct
 python fact_checker.py \
     --baseline \
-    --input_file ./fact_checking_dataset/fact_checking_dataset_${YEAR}.jsonl \
-    --output_file ./results/fact_checking_dataset_${YEAR}_${MODEL}_baseline.jsonl \
-    --log_file ./results/fact_checking_dataset_${YEAR}_${MODEL}_baseline.log \
+    --input_file ./fact_checking_dataset/hotpotqa_subset_judged.jsonl \
+    --output_file ./results/hotpotqa_subset_judged_${MODEL}_baseline.jsonl \
+    --log_file ./results/hotpotqa_subset_judged_${MODEL}_baseline.log \
     --model_path /workspace/HFModels/${MODEL} \
     --evaluate
 
-echo "Baseline run completed. Results saved to ./results/fact_checking_dataset_${YEAR}_${MODEL}_baseline.jsonl"
+echo "Baseline run completed. Results saved to ./results/hotpotqa_subset_judged_${MODEL}_baseline.jsonl"
